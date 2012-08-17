bbe089d300971813b61495a9aa7c7359bdfe3cbd
hugo <hugokinghall@gmail.com>
2012-08-17 13:21:02 +0100

    starting this project

---
+++ src/hugeeee/models/User.java
@@ -0,0 +1,17 @@
+package hugeeee.models;
+
+public class User {
+
+	
+	private String name;
+	private String email;
+	private String password;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
